9833fc0275f4702463e3f8a3c9034087392e4101
Sarah Haslip <103548776+shaslip@users.noreply.github.com>
2025-12-04 19:40:15 -0800

    Create BatchTranslateHandler.php

---
+++ src/Rest/BatchTranslateHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace MediaWiki\Extension\GeminiTranslator\Rest;
+
+use MediaWiki\Extension\GeminiTranslator\PageTranslator;
+use MediaWiki\Rest\SimpleHandler;
+use Wikimedia\ParamValidator\ParamValidator;
+
+class BatchTranslateHandler extends SimpleHandler {
+
+	private PageTranslator $translator;
+
+	public function __construct( PageTranslator $translator ) {
+		$this->translator = $translator;
+	}
+
+	public function execute() {
+		$body = $this->getValidatedBody();
+		$strings = $body['strings'];
+		$targetLang = $body['targetLang'];
+
+		// Limit batch size for safety
+		if ( count( $strings ) > 50 ) {
+			$strings = array_slice( $strings, 0, 50 );
+		}
+
+		$translations = $this->translator->translateStrings( $strings, $targetLang );
+
+		return $this->getResponseFactory()->createJson( [
+			'translations' => $translations
+		] );
+	}
+
+	public function getBodyParamSettings(): array {
+		return [
+			'strings' => [
+				self::PARAM_SOURCE => 'body',
+				ParamValidator::PARAM_TYPE => 'array',
+				ParamValidator::PARAM_REQUIRED => true,
+			],
+			'targetLang' => [
+				self::PARAM_SOURCE => 'body',
+				ParamValidator::PARAM_TYPE => 'string',
+				ParamValidator::PARAM_REQUIRED => true,
+			]
+		];
+	}
+}
